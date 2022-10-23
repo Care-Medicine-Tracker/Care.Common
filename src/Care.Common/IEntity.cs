13260d02c542e423c64936b7cd2003064fc7343f
vcaf <victoria.fong.0408@gmail.com>
2022-10-23 18:10:32 +0200

    first commit

---
+++ src/Care.Common/IEntity.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace Care.Common
+{
+    public interface IEntity
+    {
+        Guid Id { get; set; }
+    }
+}
---
+++ src/Care.Common/Settings/ServiceSettings.cs
@@ -0,0 +1,7 @@
+namespace Care.Common.Settings
+{
+    public class ServiceSettings
+    {
+        public string ServiceName { get; init; }
+    }
+}
